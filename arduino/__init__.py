516ee0a761b72e6b55f992fb5a20777f689ac386
Patrick Huang <huangpatrick16777216@gmail.com>
2021-11-22 18:46:39 -0800

    Bump version.

--- arduino/__init__.py
+++ arduino/__init__.py
@@ -17,7 +17,7 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-__version__ = "0.0.4"
+__version__ = "0.0.6"
 
 from .core import *
 from . import motor
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("requirements.txt", "r") as file:
 
 setuptools.setup(
     name="python-arduino",
-    version="0.0.4",
+    version="0.0.6",
     author="Patrick Huang",
     author_email="huangpatrick16777216@gmail.com",
     description="An API for controlling Arduino boards.",
